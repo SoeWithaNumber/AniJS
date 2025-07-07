af073abf2cea3d97fb0f7f7835e4460e9b8a2cc6
Tyler <tysisson@comcast.net>
2025-07-07 15:56:40 +0900

    Updated eslint config to include jest globals

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,5 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], rules:{eqeqeq: "off"} },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{test,spec}.js"], languageOptions: {globals: globals.jest} }
 ]);
